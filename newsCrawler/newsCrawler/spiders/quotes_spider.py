c9c95ccf121af899f623bc782661569c83b24b56
MohammadrezaPourreza <m1378.prz@gmail.com>
2020-11-19 00:29:05 +0330

    items output

--- newsCrawler/newsCrawler/spiders/quotes_spider.py
+++ newsCrawler/newsCrawler/spiders/quotes_spider.py
@@ -4,7 +4,7 @@ from ..items import NewscrawlerItem
 class QuoteSpider(scrapy.Spider):
     name = 'firstTry'
     start_urls = [
-        'https://www.truthorfiction.com/',
+        'https://www.truthorfiction.com/page/1/',
     ]
 
     def _parse(self, response, **kwargs):
@@ -13,11 +13,13 @@ class QuoteSpider(scrapy.Spider):
         titles = response.css('.entry-title a::text').extract()
         authors = response.css('.author-name::text').extract()
         dates = response.css('.published::text').extract()
+        for i in range(len(titles)):
+            items['titles'] = titles[i]
+            items['authors'] = authors[i]
+            items['dates'] = dates[i]
+            yield items
 
-        items[titles] = titles
-        items[authors] = authors
-        items[dates] = dates
-        yield items
+
       #  for i in range(len(titles)):
       #      yield {
       #          'title': titles[i],
