eed1f5a3c6ea30e829442e0020afc3ee0adf5d1f
rezaBarzgar <r.barzgar98@gmail.com>
2020-11-19 11:02:50 +0330

    reza pushing

--- newsCrawler/newsCrawler/spiders/quotes_spider.py
+++ newsCrawler/newsCrawler/spiders/quotes_spider.py
@@ -17,12 +17,12 @@ class QuoteSpider(scrapy.Spider):
         titles = response.css('.entry-title a::text').extract()
         authors = response.css('.author-name::text').extract()
         dates = response.css('.published::text').extract()
-        page_Url = 'https://www.truthorfiction.com/page/'+str(QuoteSpider.page_number-1)+'/'
+        page_Urls = response.css('.entry-title a').xpath("@href").extract()
         for i in range(len(titles)):
             items['titles'] = titles[i]
             items['authors'] = authors[i]
             items['dates'] = dates[i]
-            items['page_Url'] = page_Url
+            items['page_Url'] = page_Urls[i]
             yield items
 
         next_page = 'https://www.truthorfiction.com/page/'+str(QuoteSpider.page_number)+'/'
