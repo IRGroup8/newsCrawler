85c3e8c632ee30ec010607650655e5ebc9dd153c
Mohammadreza Pourreza <71866535+MohammadrezaPourreza@users.noreply.github.com>
2020-11-19 09:37:52 +0330

    Update quotes_spider.py

--- newsCrawler/newsCrawler/spiders/quotes_spider.py
+++ newsCrawler/newsCrawler/spiders/quotes_spider.py
@@ -4,24 +4,14 @@ from ..items import NewscrawlerItem
 
 class QuoteSpider(scrapy.Spider):
 
-    name = 'newsCrawler'
-    page_no = 2
-    start_urls = ['https://www.politifact.com/article/list/', 'https://www.truthorfiction.com', 'https://www.snopes.com/fact-check/']
+    name = 'firstTry'
+    page_number = 2
+    allowed_domains = ['truthorfiction.com']
+    start_urls = [
+        'https://www.truthorfiction.com/page/1/',
+    ]
 
-
-    def set_url(self, url, page_no):
-
-        if url == self.start_urls[0]:            # www.politifact.com
-            return url + "?page=" + str(page_no)
-
-        elif url == self.start_urls[1]:          # www.truthorfiction.com
-            return url + "/page/" + str(page_no) + '/'
-
-        elif url == self.start_urls[1]:          # www.snopes.com
-            return url + "page/" + str(page_no) + '/'
-
-
-    def parse(self, response, **kwargs):
+    def parse(self, response):
         items = NewscrawlerItem()
 
         titles = response.css('.entry-title a::text').extract()
@@ -33,12 +23,10 @@ class QuoteSpider(scrapy.Spider):
             items['dates'] = dates[i]
             yield items
 
-        next_page = self.set_url(url=self.start_urls[0], page_no=self.page_no)
-        if self.page_no < 515:
-            self.page_no += 1
+        next_page = 'https://www.truthorfiction.com/page/'+str(QuoteSpider.page_number)+'/'
+        if QuoteSpider.page_number < 515:
+            QuoteSpider.page_number += 1
             yield response.follow(next_page, callback=self.parse)
-
-
 
       #  for i in range(len(titles)):
       #      yield {
@@ -46,3 +34,5 @@ class QuoteSpider(scrapy.Spider):
       #          'author': authors[i],
       #          'date': dates[i]
       #      }
+
+
