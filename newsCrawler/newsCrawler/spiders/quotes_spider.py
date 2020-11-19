095553b6e39887544a30047c81d8f6a8c6b9744b
Sobii <moradiyan.sobhan@gmail.com>
2020-11-19 12:45:26 +0330

    Final edition ...

--- newsCrawler/newsCrawler/spiders/quotes_spider.py
+++ newsCrawler/newsCrawler/spiders/quotes_spider.py
@@ -3,12 +3,10 @@ from ..items import NewscrawlerItem
 
 
 class QuoteSpider(scrapy.Spider):
-    name = 'firstTry'
-    page_number = 2
+    name = 'newsCrawler'
     allowed_domains = ['truthorfiction.com']
-    start_urls = [
-        'https://www.truthorfiction.com/page/1/',
-    ]
+    start_urls = ['https://www.truthorfiction.com/page/1/', ]
+
 
     def parse_news(self, response):
         items = NewscrawlerItem()
@@ -19,6 +17,7 @@ class QuoteSpider(scrapy.Spider):
         tags = response.css(".rating-description span::text").extract()
         content = response.css(".entry-content p::text").extract()
         cats = response.css('.cat-links a::text').extract()
+
         # adding to item
         items['titles'] = titles
         items['authors'] = authors
@@ -27,15 +26,17 @@ class QuoteSpider(scrapy.Spider):
         items['tags'] = tags
         items['content'] = content
         items['cats'] = cats
+
         yield items
 
+    page_number = 2
     def parse(self, response):
         page_Urls = response.css('.entry-title a').xpath("@href").extract()
         for i in range(len(page_Urls)):
-            yield response.follow(page_Urls[i],callback=self.parse_news)
+            yield response.follow(page_Urls[i], callback=self.parse_news)
 
-        next_page = 'https://www.truthorfiction.com/page/'+str(QuoteSpider.page_number)+'/'
-        if QuoteSpider.page_number < 12:
-            QuoteSpider.page_number += 1
+        next_page = 'https://www.truthorfiction.com/page/' + str(self.page_number) + '/'
+        if self.page_number < 12:
+            self.page_number += 1
             yield response.follow(next_page, callback=self.parse)
 
